f71b607bb6c9dd63bcfd9c4b0f95cdbb093883f3
Darun Bjork <139690402+darunbjork@users.noreply.github.com>
2024-07-17 22:53:51 +0200

    Removed Dark Mode button and related code, and included resume download

--- public/js/main.js
+++ public/js/main.js
@@ -1,56 +1,45 @@
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("DOM fully loaded and parsed");
-  
-    // Smooth scrolling for navigation links
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function(e) {
-            e.preventDefault();
-            document.querySelector(this.getAttribute('href')).scrollIntoView({
-                behavior: 'smooth'
-            });
-        });
-    });
-  
-    // Dark mode toggle
-    const darkModeToggle = document.getElementById('dark-mode-toggle');
-    console.log(darkModeToggle);
-    darkModeToggle.addEventListener('click', () => {
-        console.log("Dark mode toggle clicked");
-        document.body.classList.toggle('dark-mode');
-    });
-  
-    // Scroll-to-top button
-    const scrollToTopButton = document.getElementById('scroll-to-top');
-    window.addEventListener('scroll', () => {
-        if (window.scrollY > 300) {
-            scrollToTopButton.style.display = 'block';
-        } else {
-            scrollToTopButton.style.display = 'none';
-        }
-    });
-  
-    scrollToTopButton.addEventListener('click', () => {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    });
+  console.log("DOM fully loaded and parsed");
+
+  // Smooth scrolling for navigation links
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function(e) {
+          e.preventDefault();
+          document.querySelector(this.getAttribute('href')).scrollIntoView({
+              behavior: 'smooth'
+          });
+      });
   });
 
-  document.addEventListener("DOMContentLoaded", function() {
-    const techItems = document.querySelectorAll('.slide-in');
-  
-    const observer = new IntersectionObserver((entries, observer) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add('visible');
-          observer.unobserve(entry.target);
-        }
-      });
-    }, {
-      threshold: 0.1
-    });
-  
-    techItems.forEach(item => {
-      observer.observe(item);
-    });
+
+  // Scroll-to-top button
+  const scrollToTopButton = document.getElementById('scroll-to-top');
+  window.addEventListener('scroll', () => {
+      if (window.scrollY > 300) {
+          scrollToTopButton.style.display = 'block';
+      } else {
+          scrollToTopButton.style.display = 'none';
+      }
   });
-  
-  
+
+  scrollToTopButton.addEventListener('click', () => {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
+
+  const techItems = document.querySelectorAll('.slide-in');
+
+  const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              entry.target.classList.add('visible');
+              observer.unobserve(entry.target);
+          }
+      });
+  }, {
+      threshold: 0.1
+  });
+
+  techItems.forEach(item => {
+      observer.observe(item);
+  });
+});
